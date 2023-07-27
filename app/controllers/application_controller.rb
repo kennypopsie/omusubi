b25be6317f674e915f416c330a9f74ecf6035528
kensuke <mynameiskensuke@gmail.com>
2023-07-27 01:14:16 +0000

    認証ページ作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
  # この行を追加
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   #Channel Controller
-  get 'channel/new', to: 'channel#new', as: 'new_channel'
+  get '/', to: 'channel#new', as: 'new_channel'
 
 end
