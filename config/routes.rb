528cdaa1ffe81c5d6c9eaa97a38a996d293c33f8
kensuke <mynameiskensuke@gmail.com>
2023-07-28 01:00:37 +0000

    nameとprofileカラム追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users#, controllers: { registrations: 'users/registrations' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   #Channel Controller
   get '/', to: 'channel#new', as: 'new_channel'
+  
+
+  get 'users/show', to: 'users#show', as: 'show_user'
 
 end
---
+++ db/migrate/20230727082655_add_name_prpfile_to_users.rb
@@ -0,0 +1,5 @@
+class AddNamePrpfileToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :name, :string
+  end
+end
