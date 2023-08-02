93014b80bf2f604cb9e39f84b164a2d92bab3e1b
kensuke <mynameiskensuke@gmail.com>
2023-08-02 02:31:51 +0000

    モーダルウィンドウ追加

--- app/javascript/modal-window.js
+++ app/javascript/modal-window.js
@@ -1,15 +1,23 @@
-  const modal = document.querySelector('.js-modal');
-  const modalButton = document.querySelector('.js-modal-button');
+document.addEventListener('DOMContentLoaded', (event) => {
+  // モーダルを開くボタンを全て取得
+  const buttons = document.querySelectorAll('.js-modal-button');
 
-
-  const modalClose = document.querySelector('.js-close-button');
-
-  modalButton.addEventListener('click', () => {
-    modal.classList.add('is-open');
-
+  buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+      // ボタンのdata-target属性からモーダルのIDを取得
+      const target = button.dataset.target;
+      // モーダルに is-open クラスを追加
+      document.querySelector(target).classList.add('is-open');
+    });
   });
 
-  modalClose.addEventListener('click', () => {
-    modal.classList.remove('is-open');
+  // モーダルを閉じるボタンを全て取得
+  const closeButtons = document.querySelectorAll('.js-close-button');
 
+  closeButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+      // ボタンが含まれるモーダルから is-open クラスを削除
+      button.closest('.js-modal').classList.remove('is-open');
+    });
   });
+});
