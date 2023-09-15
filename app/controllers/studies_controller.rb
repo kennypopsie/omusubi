161349cc3965f547d2ed1bb852980357b518fe36
kensuke <mynameiskensuke@gmail.com>
2023-09-15 06:13:24 +0000

    学習終了ボタン

--- app/controllers/studies_controller.rb
+++ app/controllers/studies_controller.rb
@@ -2,26 +2,39 @@ class StudiesController < ApplicationController
     before_action :authenticate_user!
     
   def new
+    @study = Study.new
   end
   
   def index
+    @studies = Study.all
+    @study = Study.find(params[:id])
   end
 
   def create
     @study = Study.new
     @study.user_id = current_user.id
     @study.start_time = Time.now
-
     if @study.save
-      # 保存が成功した場合の処理
-      redirect_to studies_path, notice: 'Current time recorded.'
+      # idをリダイレクト先に渡す
+      redirect_to studies_path(id: @study.id), notice: 'Current time recorded.'
     else
-      # 保存が失敗した場合の処理
       redirect_to studies_path, alert: 'Failed to record current time.'
     end
   end
   
   def update
+    @study = Study.find(params[:id])
+    # @comment = Comment.find(params[:id])
+    if @study.update(end_time: Time.now)    
+      redirect_to request.referer, notice: '更新しました'
+    else
+      redirect_to request.referer, status: :unprocessable_entity
+    end 
   end
+  
+  # private
+  # def study_params
+  #   params.permit(:id)
+  # end
   
 end
